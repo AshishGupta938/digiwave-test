5c736018f25ff453428419cf09f61241703bae8c
Ashish Gupta <ashvgupta938@gmail.com>
2022-10-21 22:02:44 -0400

    Testing

---
+++ flash.js
@@ -0,0 +1,15 @@
+var imageCapture = null;
+
+var deviceConfig = {
+    video: {
+        width: 480,
+        height: 640,
+        facingMode: "environment",
+        deviceId: null
+    }
+};
+
+var imageCaptureConfig = {
+    fillLightMode: "torch",
+    focusMode: "continuous"
+};
